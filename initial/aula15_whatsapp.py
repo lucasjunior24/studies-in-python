bbf0144aa4e2c59561f9612208b6fdde4972c98e
Souza <lucas11souza97@gmail.com>
2025-03-13 21:51:40 -0400

    feat: added new class

---
+++ initial/aula15_whatsapp.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+# Send a WhatsApp Message to a Contact at 1:30 PM
+
+pywhatkit.sendwhatmsg_instantly("+5569981120993", "Hi Lucas")
+# Same as above but Closes the Tab in 2 Seconds after Sending the Message
+# pywhatkit.sendwhatmsg("+5569981120993", "Hi Lucas")
